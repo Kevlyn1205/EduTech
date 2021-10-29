d64b03acc075ec8f5ba773dd86aa5d6ae3f87d03
Kevin Pérez <kevlyn1205@hotmail.com>
2021-10-29 10:23:59 -0600

    API

---
+++ WebApplication3/Controllers/ClienteController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication3.Modelo;
+
+namespace WebApplication3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        private readonly RepositorioClientes repositorioClientes;
+        
+        public ClienteController()
+        {
+            repositorioClientes = new RepositorioClientes();
+        }
+        [HttpGet]
+        public IEnumerable<Clientes> Get()
+        {
+            return repositorioClientes.GetAll();
+        }
+        [HttpGet("{Codigo}")]
+        public Clientes Get(int Codigo)
+        {
+            return repositorioClientes.GetById(Codigo);
+        }
+        [HttpPost]
+        public void Post([FromBody]Clientes clnts)
+        {
+            if (ModelState.IsValid)
+                repositorioClientes.Add(clnts);
+        }
+        [HttpPut("{Codigo}")]
+        public void Put(int Codigo,[FromBody]Clientes clnts)
+        {
+            clnts.Codigo = Codigo;
+            if (ModelState.IsValid)
+                repositorioClientes.Update(clnts);
+        }
+        [HttpDelete("{Codigo}")]
+        public void Delete(int Codigo)
+        {
+            repositorioClientes.Delete(Codigo);
+        }
+
+    }
+}
---
+++ WebApplication3/Modelo/RepositorioClientes.cs
@@ -0,0 +1,73 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Modelo
+{
+    public class RepositorioClientes
+    {
+        private string connectionString;
+        public RepositorioClientes()
+        {
+            connectionString = @"Integrated Security=true; Initial Catalog=Clientes; Data Source=DESKTOP-TCFT49L; ";
+
+        }
+        public IDbConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(connectionString);
+            }
+        }
+        public void Add(Clientes clnts)
+        {
+            using(IDbConnection dbConnection=Connection)
+            {
+                string sQuery = @"INSERT INTO Clientes (Nombre, Telefono, Email) VALUES (@Nombre, @Telefono, @Email)";
+                Connection.Open();
+                Connection.Execute(sQuery, clnts);
+            }
+        }
+
+        public IEnumerable<Clientes> GetAll()
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                string sQuery = @"SELECT * FROM Clientes";
+                dbConnection.Open();
+                return dbConnection.Query<Clientes>(sQuery);
+            }
+        }
+        public Clientes GetById(int Codigo)
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                string sQuery = @"SELECT * FROM Clientes WHERE Codigo=@Codigo";
+                dbConnection.Open();
+                return dbConnection.Query<Clientes>(sQuery, new { Id = Codigo }).FirstOrDefault();
+            }
+        }
+        public void Delete(int Codigo)
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                string sQuery = @"DELETE FROM Clientes WHERE Codigo=@Codigo";
+                dbConnection.Open();
+                dbConnection.Execute(sQuery,new { Codigo = Codigo });
+            }
+        }
+        public void Update(Clientes clie)
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                string sQuery = @"UPDATE Clientes SET Nombre=@Nombre, Telefono=@Telefono, Email=@Email WHERE Codigo=@Codigo";
+                dbConnection.Open();
+                dbConnection.Query<Clientes>(sQuery, clie);
+            }
+        }
+    }
+}
